2b0bb761abce70edb6c3547cada422ef54e1d7f3
Melody Truong <mtruong@dropbox.com>
2021-07-13 23:58:31 -0700

    Add EmailTable

--- backend/register_lambda.py
+++ backend/register_lambda.py
@@ -1,10 +1,26 @@
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
 def lambda_handler(event, context):
+    params = event['queryStringParameters']
+    email = params['email']
+    password = params['password']
+
+    dynamodb.put_item(
+        TableName='Emails',
+        Item={
+            'email': {'S': email},
+            'password': {'S': password},
+        }
+    )
+
     response = {
             'statusCode': 200,
             'headers': {
                 'Access-Control-Allow-Origin': '*'
             },
-            'body': ""
+            'body': 'Email {} registered!'.format(email)
         }
 
     print('Response: %s' % (response))
