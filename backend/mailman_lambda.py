a8b1c68f505434b33228a3c4da17f9cc2f238d71
Melody Truong <mtruong@dropbox.com>
2021-07-14 17:07:01 -0700

    Add Dropbox Bundle and Integrate with Dropbox Client

--- backend/mailman_lambda.py
+++ backend/mailman_lambda.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dropbox_client import MailmanDropboxClient
 from fetch_email import EmailFetcher
 import boto3
 import os
@@ -9,26 +10,29 @@ PASSWORD = os.environ['password']  # Password for email account
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    print('Fetching emails for {} at {}...'.format(EMAIL, event['time']))
+    print('Requesting emails for {} at {}...'.format(EMAIL, event['time']))
     num_emails = 0
     try:
+        dropbox_client = MailmanDropboxClient()
         fetcher = EmailFetcher()
         emails = fetcher.fetch_account_emails(EMAIL, PASSWORD)
         num_emails = len(emails)
 
         for email in emails:
             sender = email.from_
+            print('Processing email from {}...'.format(sender))
             response = dynamodb.get_item(
                 TableName='Registrations',
                 Key={'email': {'S': sender}},
             )
             item = response['Item']
             oauth_token = item['dropbox_oauth_token']['S']
-            # TODO: Use oauth_token to make Dropbox files_upload request
+            dropbox_client.upload_file_to_dropbox(email.text, '/Mailman', email.subject + '.txt', oauth_token)
+            print('Email from {} successfully uploaded to Dropbox!'.format(sender))
     except:
-        print('Fetch failed!')
+        print('Request failed!')
         raise
     else:
-        print('Fetch succeeded! {} emails found'.format(num_emails))
+        print('Request succeeded! {} emails found'.format(num_emails))
     finally:
-        print('Fetch completed at {}'.format(str(datetime.now())))
+        print('Request completed at {}'.format(str(datetime.now())))
--- cloudformation/update-development-stack.sh
+++ cloudformation/update-development-stack.sh
@@ -2,11 +2,11 @@ cd backend
 zip --symlinks -r register.zip register_lambda.py
 aws s3 cp register.zip s3://mailman-deployment-bucket/build/register.zip
 
-pip install --target ./package imap_tools
+pip install --target ./package dropbox imap_tools
 cd package
 zip -r ../mailman.zip .
 cd ..
-zip -g mailman.zip config.py fetch_email.py mailman_lambda.py
+zip -g mailman.zip config.py dropbox_client.py fetch_email.py mailman_lambda.py
 aws s3 cp mailman.zip s3://mailman-deployment-bucket/build/mailman.zip
 
 cd ../cloudformation
